5246a7976b2d2a7491b57c0f1067fbc9ee92baac
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2014-10-13 18:33:01 +0000

    setup app init dependencies and mockup datasources

---
+++ Kendo UI Mobile To-Do/app/datasources/todosDataSource.js
@@ -0,0 +1,18 @@
+define([], function () {
+    return new kendo.data.DataSource({
+        data: [
+            {
+                title: "Talk to corporate",
+                category: "Work"
+            },{
+                title: "Promote synergy",
+                category: "Work"
+            },{
+                title: "Find out what synergy is",
+                category: "Personal"
+            },{
+                title: "Eat some pot de creme",
+                category: "Personal"
+            }]
+    });
+});
