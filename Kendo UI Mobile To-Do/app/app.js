c5bb520de6bc5855ee5e67ec3aa4220cb29eb907
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2014-10-13 19:39:44 +0000

    parsing todo html templates

--- Kendo UI Mobile To-Do/app/app.js
+++ Kendo UI Mobile To-Do/app/app.js
@@ -1,20 +1,26 @@
 define([
-    'datasources/categoriesDatasource',
-    'datasources/todosDataSource'
-], function(catDataSrc, todosDataSource){
-   
+    'datasources/categoriesDataSource',
+    'datasources/todosDataSource',
+], function (catDataSrc, todosDataSource) {
+
     var app = {
-        init: function(){
-            this.instance = new kendo.mobile.Application(document.body, {
-                skin: 'flat',
-                initialView: 'todos'
-            });  
+        init: function () {
+            var self = this;
+
+            require(['views/todos/todos'], function () {
+                self.instance = new kendo.mobile.Application(document.body, {
+                    skin: 'flat',
+                    initialView: 'todos'
+                });
+            });
+
+
         },
         data: {
             categories: catDataSrc,
             todos: todosDataSource
         }
     };
-    
+
     return app;
 });
